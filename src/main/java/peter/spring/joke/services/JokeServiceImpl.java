791c9f93fd3046198c12a9ef1fb2da626572c512
pchen <pchen@indeed.com>
2019-01-10 09:28:29 -0800

    master
    Added comments to explain how JokeServiceImpl
    ensures that no dependency injection is triggered
    here and the variable "chuckNorrisQuotes" is
    initialized exactly once!

--- src/main/java/peter/spring/joke/services/JokeServiceImpl.java
+++ src/main/java/peter/spring/joke/services/JokeServiceImpl.java
@@ -6,10 +6,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class JokeServiceImpl implements JokeService {
 
-    // BY setting up the constructor this way, this class will be
+    // By setting up the constructor in this way, this class will be
     // created exactly once.
+    // The "final" qualifier will ensure this variable is initialized exactly once
+    // even if this class is called multiple times.
     private final ChuckNorrisQuotes chuckNorrisQuotes;
     public JokeServiceImpl() {
+        // Here, we don't want Spring to manage this as a bean for as, and therefore
+        // we are not using dependency injection and this is not a constructor-based
+        // dependency injection.
         this.chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
 
